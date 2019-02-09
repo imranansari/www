9ddcf385cd445a97e264da156ac91ac718a8c288
z0mbie42 <sylvain@kerkour.com>
2019-02-09 21:11:52 +0100

    fix tslint minor error

--- src/views/platform/Index.vue
+++ src/views/platform/Index.vue
@@ -2,3 +2,22 @@
   <blm-platform-dashboard v-if="$store.state.session" />
   <blm-landing-platform v-else />
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+
+@Component
+export default class Platform extends Vue {
+  // props
+  // data
+  // computed
+  // lifecycle
+  // watch
+  // methods
+}
+</script>
+
+
+<style scoped lang="scss">
+</style>
